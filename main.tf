60d94bf763624d989b87fc380f7e7be054e2e68f
bantawit <75023446+bantawit@users.noreply.github.com>
2024-12-29 01:58:46 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,69 @@
+provider "aws" {
+  region = "us-east-1"  # Change the region as needed
+}
+
+# 1. Create the Permissions Boundary Policy
+resource "aws_iam_policy" "permissions_boundary_policy" {
+  name        = "${var.user_name}_CustomIPWhitelist"
+  description = "Permissions Boundary for IP Whitelist"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "*"
+        Resource = "*"
+        Condition = {
+          "ForAnyValue:IpAddress" = {
+            "aws:SourceIp" = var.ip_addresses
+          }
+        }
+      }
+    ]
+  })
+}
+
+# 2. Create the IAM User
+resource "aws_iam_user" "new_user" {
+  name = var.user_name
+}
+
+# 3. Create Inline Policy for the User
+resource "aws_iam_user_policy" "user_policy" {
+  user   = aws_iam_user.new_user.name
+  name   = "${var.user_name}_policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = "sts:AssumeRole"
+        Effect   = "Allow"
+        Resource = "arn:aws:iam::${var.account_id}:role/${var.role_name}"
+      }
+    ]
+  })
+}
+
+# 4. Attach Permissions Boundary to the User
+resource "aws_iam_user_permissions_boundary" "user_permissions_boundary" {
+  user                = aws_iam_user.new_user.name
+  permissions_boundary = aws_iam_policy.permissions_boundary_policy.arn
+}
+
+# 5. Create Access Keys for the User
+resource "aws_iam_access_key" "user_access_key" {
+  user = aws_iam_user.new_user.name
+}
+
+# 6. Output the IAM User and Access Key Details
+output "user_name" {
+  value = aws_iam_user.new_user.name
+}
+
+output "access_key_id" {
+  value = aws_iam_access_key.user_access_key.id
+}
+
+output "access_key_secret" {
+  value = aws_iam_access_key.user_access_key.secret
+}
